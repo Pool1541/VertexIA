48418ebce7d5071f7f02b6155cd1967cd66d83e1
Pool <pool_1541@hotmail.com>
2023-11-12 22:43:47 -0500

    add file with vision ai methos

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-const vision = require('@google-cloud/vision');
+import vision from '@google-cloud/vision';
+import { labelDetection } from './src/label_detection.js';
 
-const TRANSLATED_RESULTS = {
-  VERY_UNLIKELY: 'MUY IMPROBABLE',
-  UNLIKELY: 'IMPROBABLE',
-  VERY_LIKELY: 'MUY PROBABLE',
-  LIKELY: 'PROBABLE',
-};
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: 'google-cloud-key.json',
+});
 
-async function analyzeImage(image) {
-  try {
-    const cliente = new vision.ImageAnnotatorClient({
-      keyFilename: 'google-cloud-key.json',
-    });
-
-    // const [result] = await cliente.labelDetection(image);
-    const [results] = await cliente.safeSearchDetection(image);
-    const detections = results.safeSearchAnnotation;
-
-    return {
-      humor: TRANSLATED_RESULTS[detections.spoof] || detections.spoof,
-      sexual: TRANSLATED_RESULTS[detections.adult] || detections.adult,
-      medico: TRANSLATED_RESULTS[detections.medical] || detections.medical,
-      atrevido: TRANSLATED_RESULTS[detections.racy] || detections.racy,
-      violento: TRANSLATED_RESULTS[detections.violence] || detections.violence,
-    };
-  } catch (error) {
-    throw error;
-  }
-}
-
-analyzeImage('./images/wound.jpg')
+labelDetection({ client, imagePath: './images/cat.jpg' })
   .then(console.log)
   .catch((error) => console.error('Error al analizar la imagen:', error));
---
+++ src/visionAPIMethods.js
@@ -0,0 +1,28 @@
+import { TRANSLATED_RESULTS } from './constants.js';
+
+export async function labelDetection({ client, imagePath }) {
+  try {
+    const [results] = await client.labelDetection(imagePath);
+    const labels = results.labelAnnotations;
+    return labels;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function safeSearch({ client, imagePath }) {
+  try {
+    const [results] = await client.safeSearchDetection(imagePath);
+    const detections = results.safeSearchAnnotation;
+
+    return {
+      humor: TRANSLATED_RESULTS[detections.spoof] || detections.spoof,
+      sexual: TRANSLATED_RESULTS[detections.adult] || detections.adult,
+      medico: TRANSLATED_RESULTS[detections.medical] || detections.medical,
+      atrevido: TRANSLATED_RESULTS[detections.racy] || detections.racy,
+      violento: TRANSLATED_RESULTS[detections.violence] || detections.violence,
+    };
+  } catch (error) {
+    throw error;
+  }
+}
